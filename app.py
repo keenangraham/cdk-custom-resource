cbce5f6fe0a751c6ea84373c52c2f5a72076aab3
Keenan Graham <ksgraham@stanford.edu>
2022-05-10 21:24:48 -0700

    Clean

--- app.py
+++ app.py
@@ -3,26 +3,17 @@ import os
 
 import aws_cdk as cdk
 
-from cdk_custom_resource.cdk_custom_resource_stack import CdkCustomResourceStack
+from shared_infrastructure.cherry_lab import US_WEST_2
+
+from cdk_custom_resource.cdk_custom_resource_stack import CustomResourceStack
 
 
 app = cdk.App()
-CdkCustomResourceStack(app, "CdkCustomResourceStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
 
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+CustomResourceStack(
+    app,
+    'CdkCustomResourceStack',
+    env=US_WEST_2,
+)
 
 app.synth()

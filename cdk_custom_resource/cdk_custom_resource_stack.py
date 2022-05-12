50fe1e660a9813567b78877eedce13efd18d7458
Keenan Graham <ksgraham@stanford.edu>
2022-05-12 15:22:39 -0700

    Allow role to describe db snapshots

--- cdk_custom_resource/cdk_custom_resource_stack.py
+++ cdk_custom_resource/cdk_custom_resource_stack.py
@@ -9,12 +9,15 @@ from constructs import Construct
 from aws_cdk.aws_lambda_python_alpha import PythonFunction
 from aws_cdk.aws_lambda import Runtime
 
+from aws_cdk.aws_iam import PolicyStatement
+
+
 class CustomResourceStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        PythonFunction(
+        get_latest_rds_snapshot_id = PythonFunction(
             self,
             'GetLatestRDSSnapshotID',
             entry='cdk_custom_resource/runtime/lambda/',
@@ -22,4 +25,11 @@ class CustomResourceStack(Stack):
             index='functions.py',
             handler='get_latest_rds_snapshot_id',
             timeout=Duration.seconds(60),
+        )
+
+        get_latest_rds_snapshot_id.role.add_to_policy(
+            PolicyStatement(
+                actions=['rds:DescribeDBSnapshots'],
+                resources=['*'],
+            )
         )
